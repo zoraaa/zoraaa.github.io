d6287755aa2d1e322ed87ed397f1c5c0ea3912fc
zora <zora@zoradeMacBook-Pro.local>
2020-04-12 22:05:43 +0800

    fix

---
+++ themes/default/source/js/main.js
@@ -0,0 +1,7 @@
+$(function () {
+    $(".in_profileItem a").hover(function () {
+        $(this).stop().animate({ top: "-20px", opacity: ".7" });
+    }, function () {
+        $(this).stop().animate({ top: "0", opacity: "1" });
+    })
+})
